c3580a5a64d9e1f3d18b91c19ddf64d1756d4e8d
Daniel Foote <danfoote104227@gmail.com>
2021-02-07 23:24:47 -0500

    Added user registration & login.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('content')
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="remember_me">
+            <label class="form-check-label" for="remember_me" name="remember">Remember Me</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@stop
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('content')
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" name="name" aria-describedby="nameHelp" required>
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" required>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+        <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Password Confirmation</label>
+            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+@stop
